2a923d2faf2764c9850dd0a86f0ff833403db3d8
Hamdanirevou <hamdanie100103@gmail.com>
2023-10-06 21:24:52 +0700

    finishing display design

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 // Toggle class active
 const navbarNav = document.querySelector('.navbar-nav');
 // ketika menu diklik
-document.querySelector('#hamburger-menu').onclick = () =>{
+document.querySelector('#hamburger-menu').onclick = (e) =>{
     navbarNav.classList.toggle('active');
+    e.preventDefault();
 }
 
 //klik luar side bar untuk menghilangkan nav
