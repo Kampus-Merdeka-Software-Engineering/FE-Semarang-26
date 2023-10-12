52c890d08b3117c426b4334c5c3ec2c231da2378
hamidahmaulidaa <142779349+hamidahmaulidaa@users.noreply.github.com>
2023-10-12 11:31:21 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,120 +1,106 @@
 // Toggle class active
 const navbarNav = document.querySelector('.navbar-nav');
 // ketika menu diklik
-document.querySelector('#hamburger-menu').onclick = (e) =>{
-    navbarNav.classList.toggle('active');
-    e.preventDefault();
-}
+document.querySelector('#hamburger-menu').onclick = (e) => {
+  navbarNav.classList.toggle('active');
+  e.preventDefault();
+};
 
 //klik luar side bar untuk menghilangkan nav
 const hamburger = document.querySelector('#hamburger-menu');
-document.addEventListener('click', function(e) {
-    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)){
+document.addEventListener('click', function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove('active');
-    }
-})
+  }
+});
 
 document.addEventListener("DOMContentLoaded", function () {
   const loginForm = document.getElementById("login-form");
   const welcomeMessage = document.getElementById("welcome-message");
 
   loginForm.addEventListener("submit", function (event) {
-      event.preventDefault();
+    event.preventDefault();
 
-      const email = document.getElementById("email").value;
-      const password = document.getElementById("password").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
 
-      // Ganti URL dengan URL API yang sesuai
-      const apiUrl = "https://indigo-beaver-wrap.cyclic.app/login"; // Contoh URL API
+    // Ganti URL dengan URL API yang sesuai
+    const apiUrl = "https://indigo-beaver-wrap.cyclic.app/login"; // Contoh URL API
 
-      const requestData = {
-          email: email,
-          password: password
-      };
+    const requestData = {
+      email: email,
+      password: password
+    };
 
-      // Ubah metode HTTP menjadi POST
-      const options = {
-          method: "POST",
-          body: JSON.stringify(requestData),
-          headers: {
-              "Content-Type": "application/json"
-          }
-      };
+    // Ubah metode HTTP menjadi POST
+    const options = {
+      method: "POST",
+      body: JSON.stringify(requestData),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
 
-      // Kirim permintaan ke API
-      fetch(apiUrl, options)
+    // Kirim permintaan ke API
+    fetch(apiUrl, options)
       .then(response => response.json())
       .then(data => {
-    console.log(data)
-    // Periksa apakah login berhasil
-    if (data.success) {
-        console.log("masuk")
+        console.log(data);
+        // Periksa apakah login berhasil
+        if (data.success) {
+          console.log("masuk");
 
-        welcomeMessage.textContent = `Selamat datang, ${data.username}!`;
-        // Tambahkan kode ini untuk mengarahkan pengguna ke index.html
-        window.location.href = "https://kampus-merdeka-software-engineering.github.io/FE-Semarang-26/index.html";
-      } else {
-        console.log("ga")
-        welcomeMessage.textContent = "Login gagal. Periksa kembali username dan password Anda.";
-    }
-})
-
-
-
-
- 
-// document.addEventListener("DOMContentLoaded", function () {
-//     var formBook = document.getElementById("formBook");
-//     if (formBook) {
-//         // Akses properti 'value' dari elemen formulir di sini
-//         var value = formBook.value;
-//         console.log(value);
-//     } else {
-//         console.error("Elemen formulir tidak ditemukan.");
-//     }
-// });
-
+          welcomeMessage.textContent = `Selamat datang, ${data.username}!`;
+          // Tambahkan kode ini untuk mengarahkan pengguna ke index.html
+          window.location.href = "https://kampus-merdeka-software-engineering.github.io/FE-Semarang-26/index.html";
+        } else {
+          console.log("ga");
+          welcomeMessage.textContent = "Login gagal. Periksa kembali username dan password Anda.";
+        }
+      });
+  });
+});
 
 document.addEventListener("DOMContentLoaded", function () {
-    const formBook = document.getElementById("form-book");
-    if (formBook) {
-      formBook.addEventListener("submit", function (event) {
-        event.preventDefault();
-  
-        const first_name = document.getElementById("first_name").value;
-        const last_name = document.getElementById("last_name").value;
-        const email = document.getElementById("email").value;
-        const date = document.getElementById("date").value;
-        const select = document.getElementById("select").value;
-        const message = document.getElementById("message").value;
-  
-        console.log(first_name, last_name, email, date, select, message);
-  
-        fetch("https://indigo-beaver-wrap.cyclic.app/bookings", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json"
-          },
-          body: JSON.stringify({
-            first_name: first_name,
-            last_name: last_name,
-            email: email,
-            date: date,
-            select: select,
-            message: message
-          })
-        }).then((res) => {
-          if (res.ok) {
-            alert("Add Book an appointment Successfully!")
-          } else {
-            alert("Add Book an appointment Not Successfully!")
-          }
-          console.log(res);
-        }).catch((error) => {
-         alert('Error messages: ' + error.messages);
-        });
+  const formBook = document.getElementById("form-book");
+  if (formBook) {
+    formBook.addEventListener("submit", function (event) {
+      event.preventDefault();
+
+      const first_name = document.getElementById("first_name").value;
+      const last_name = document.getElementById("last_name").value;
+      const email = document.getElementById("email").value;
+      const date = document.getElementById("date").value;
+      const select = document.getElementById("select").value;
+      const message = document.getElementById("message").value;
+
+      console.log(first_name, last_name, email, date, select, message);
+
+      fetch("https://indigo-beaver-wrap.cyclic.app/bookings", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          first_name: first_name,
+          last_name: last_name,
+          email: email,
+          date: date,
+          select: select,
+          message: message
+        })
+      }).then((res) => {
+        if (res.ok) {
+          alert("Add Book an appointment Successfully!");
+        } else {
+          alert("Add Book an appointment Not Successfully!");
+        }
+        console.log(res);
+      }).catch((error) => {
+        console.error("Error:", error);
       });
-    } else {
-      console.error("Element with ID 'form-book' not found.");
-    }
-  });
+    });
+  } else {
+    console.error("Element with ID 'form-book' not found.");
+  }
+});
